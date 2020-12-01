2b23f4cc00934bc03cf3c80a214b32821a3e3c4d
Julio Vasquez <juliodevasquez@gmail.com>
2020-12-01 13:11:29 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="Aptodo-app">
+      <TodoList  /> 
+    </div>
+  );
+}
+
+export default App;
